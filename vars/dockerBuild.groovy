3d13b6a9c12997252d2bafeaed74779007d0937a
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-14 15:22:33 +0200

    fixing the way i write sh

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
 def call(String imageName, String imageTag, String dockerHubUser) {
-    sh """
+    sh '''
         docker build -t $(dockerHubUser)/$(imageName) .
         docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):$(imageTag)
         docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):latest
 
-    """
+    '''
 }
