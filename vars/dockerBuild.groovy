55e19e5cd56ae4ac270cc2bdda5f87dd127c1b6a
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-14 15:40:31 +0200

    add image to docker build command

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String imageTag, String dockerHubUser) {
     sh '''
-        docker build -t $(dockerHubUser)/$(imageName) .
+        docker image build -t $(dockerHubUser)/$(imageName) .
         docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):$(imageTag)
         docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):latest
     '''
