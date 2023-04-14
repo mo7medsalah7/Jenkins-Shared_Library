be3910f873fea1ebd314f8015292a49f25db3b6d
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-15 00:08:52 +0200

    add .ssh directory

--- loginToKubernetes.groovy
+++ loginToKubernetes.groovy
@@ -1,7 +1,6 @@
 def call(String loginCredentials) {
     sh """
+      [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
         ssh -o StrictHostKeyChecking=no ${loginCredentials}
-
-
     """
 }
