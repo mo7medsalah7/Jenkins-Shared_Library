f1645d8fe9a0ca07872adcadd053b4eb80b56669
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-14 16:00:42 +0200

    fixing path of Dockerfile

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String imageTag, String dokerUser) {
     sh """
-        docker image build -t ${dokerUser}/${imageName} .
+        docker image build -t ${dokerUser}/${imageName} ./spring-boot-app/
         docker image tag ${dokerUser}/${imageName} ${dokerUser}/${imageName}:${imageTag}
         docker image tag ${dokerUser}/${imageName} ${dokerUser}/${imageName}:latest
     """
