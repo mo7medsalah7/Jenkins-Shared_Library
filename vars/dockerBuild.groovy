fd9f86d1643b8b43b53a1a85a3ce1169fd1ef4df
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-14 15:56:53 +0200

    fixing issue

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String imageTag, String dokerUser) {
-    sh '''
+    sh """
         docker image build -t ${dokerUser}/${imageName} .
         docker image tag ${dokerUser}/${imageName} ${dokerUser}/${imageName}:${imageTag}
         docker image tag ${dokerUser}/${imageName} ${dokerUser}/${imageName}:latest
-    '''
+    """
 }
