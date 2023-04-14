2c7f9d7780dade7e6b7b3ee2d5b54075c152e2c6
mo7medsalah7 <m.salah.azim@gmail.com>
2023-04-14 15:16:04 +0200

    adding docker build method

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,8 @@
+def call(String imageName, String imageTag, String dockerHubUser) {
+    sh ```
+        docker build -t $(dockerHubUser)/$(imageName) .
+        docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):$(imageTag)
+        docker image tag $(dockerHubUser)/$(imageName) $(dockerHubUser)/$(imageName):latest
+
+    ```
+}
